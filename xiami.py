d4129e260e1ad066957313db018f62e96239659f
yuzhe <lazynightz@gmail.com>
2014-02-17 09:49:33 +0800

    update xiami.py

--- xiami.py
+++ xiami.py
@@ -1,6 +1,4 @@
 #encoding:utf-8
-from __future__ import unicode_literals
-
 import urllib
 
 
@@ -11,7 +9,4 @@ def caesar(location):
     result.extend([location[(avg_len + 1) * remainder:][i * avg_len + 1: (i + 1) * avg_len + 1] for i in range(num-remainder)])
     url = urllib.unquote(''.join([''.join([result[j][i] for j in range(num)]) for i in range(avg_len)]) + \
                         ''.join([result[r][-1] for r in range(remainder)])).replace('^','0')
-    print url
-
-caesar('6hAFlm%%%875_%h3%c986-55%lt%mei222%8El3_D52e141EE5lt25..FFF235.FkeEd2f137%EpF.xc994F_6mae88f9d1965-%%fio336527puy8525ea28En32iam7786%13t%e2ed34%%-u')
-
+    return url
